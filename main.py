73612d0a60fce42e4ef77889c2858222f980e67a
František Kmječ <frantisek.kmjec@gmail.com>
2024-02-14 17:26:03 +0100

    Make the photo selection automatic, make images smaller

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 from flask import Flask, render_template
 import practice_utils
+
+import os
 app = Flask(__name__)
 
 @app.get("/")
 def index():
-    return render_template('index_v5.html')
+    image_dir = "media/index"
+    images = os.listdir(os.path.join(app.static_folder, image_dir))
+    return render_template('index_v5.html', images=images, image_dir=image_dir)
 
 @app.get("/kontakt")
 def contacts():
-    return render_template('kontakt.html')
+    image_dir = "media/contacts"
+    images = os.listdir(os.path.join(app.static_folder, image_dir))
+    return render_template('kontakt.html', images=images, image_dir=image_dir)
 
 @app.get("/treninky")
 def practices():
+    image_dir = "media/practices"
+    images = os.listdir(os.path.join(app.static_folder, image_dir))
     practices = practice_utils.get_trainings()
-    return render_template('treninky.html', practices=practices)
+    return render_template('treninky.html', practices=practices, images=images, image_dir=image_dir)
 
 @app.get("/tym")
 def team():
-    return render_template("team.html")
+    image_dir = "media/about"
+    images = os.listdir(os.path.join(app.static_folder, image_dir))
+    return render_template("team.html", images=images, image_dir=image_dir)
 
 @app.get("/ultimate")
 def ultimate():
-    return render_template('ultimate.html')
+    image_dir = "media/ultimate"
+    images = os.listdir(os.path.join(app.static_folder, image_dir))
+    return render_template('ultimate.html', images=images, image_dir=image_dir)
 
 if __name__ == '__main__':
     app.run()
